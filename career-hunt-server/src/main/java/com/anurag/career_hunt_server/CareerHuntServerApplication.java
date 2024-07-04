88b3d4bb5621af796c76ca13489c4f8fe6956e08
Deepak Vishwakarma <20eg106248@anurag.edu.in>
2024-07-04 15:10:40 +0530

    'firstcommit'

---
+++ career-hunt-server/src/main/java/com/anurag/career_hunt_server/CareerHuntServerApplication.java
@@ -0,0 +1,13 @@
+package com.anurag.career_hunt_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerHuntServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerHuntServerApplication.class, args);
+	}
+
+}
---
+++ career-hunt-server/src/test/java/com/anurag/career_hunt_server/CareerHuntServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anurag.career_hunt_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerHuntServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
